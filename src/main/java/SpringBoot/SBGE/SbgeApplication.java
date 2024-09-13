3aa7f7baf4f6b0d5520f38b548a9abfea9e5aa4b
SergioOlier <solierm17@gmail.com>
2024-09-13 00:56:04 -0500

    create: SpringBoot project

---
+++ src/main/java/SpringBoot/SBGE/SbgeApplication.java
@@ -0,0 +1,13 @@
+package SpringBoot.SBGE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbgeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/SpringBoot/SBGE/SbgeApplicationTests.java
@@ -0,0 +1,13 @@
+package SpringBoot.SBGE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
